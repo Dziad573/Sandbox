cb8be37a849721df8358312762c4915534f39d7a
Dziad573 <krystiansmagacz05@gmail.com>
2023-05-22 22:39:38 +0200

    jQuery

---
+++ jQuery/jQuery.js
@@ -0,0 +1,14 @@
+/*window.onload = function(){
+    alert("affwfwwf");
+}*/
+
+
+$(document).ready(
+    function(){
+        alert("afwafw");
+    }
+);
+
+$(document).ready(function() {
+    alert("jQuery działa!");
+});
