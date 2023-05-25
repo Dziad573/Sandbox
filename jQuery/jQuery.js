6f0345f65a24a2a2564f54502c813f4eeb380d9c
Dziad573 <krystiansmagacz05@gmail.com>
2023-05-25 21:58:27 +0200

    filtr

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -8,11 +8,33 @@ $(document).ready(function() {
     $(".box>i").mouseover(
         function(){
             $(this).css("border-bottom", "2px solid red");
-        }
-        )
-        $(".box>i").mouseout(
-            function(){
-                $(this).css("border-bottom", "none");
+        });
+        
+    $(".box>i").mouseout(
+        function(){
+            $(this).css("border-bottom", "none");
+    });
+    
+    $("div:contains(ad)").css("border", "none");
+    $("#test div:nth-child(5)").css({
+        "border": "5px solid blue", 
+        "width": "200px",
+        "backgroundColor": "pink"
+    });
+    
+    $("#wynik").text("Zaznaczone: 0");
+    $("#wynik").css({
+        "border": "2px solid blue",
+        "padding": "25px 15px 0 15px"
+    });
+    $("#formularz :checkbox").click(
+        function(){
+            var ilosc = $("#formularz :checked").length;
+            if(ilosc == 0){
+                $("#wynik").text("Zaznaczone: 0");
+            }else{
+                $("#wynik").text("Zaznaczone: " + ilosc);
+            }
         }
     )
 });
