532090632b64e6db6094c439a24b976f941d61b9
Dziad573 <krystiansmagacz05@gmail.com>
2023-05-26 22:27:01 +0200

    n

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+
+    $(document).ready(function() {
     $("div#a").css("color", "blue");
     $("div#b").css("color", "red");
     $("#c").css("color", "purple");
@@ -9,7 +10,7 @@ $(document).ready(function() {
         function(){
             $(this).css("border-bottom", "2px solid red");
         });
-        
+
     $(".box>i").mouseout(
         function(){
             $(this).css("border-bottom", "none");
@@ -37,5 +38,29 @@ $(document).ready(function() {
             }
         }
     )
+    var a = 5;
+    var b = 12;
+    var c = 1.161;    
+    console.log(Math.max(a, b, c));
+    console.log(Math.floor(c));
+
+    $("#losuj").click(
+        function(){
+            var min = parseInt($("#liczba1").val());
+            var max = parseInt($("#liczba2").val());
+
+            function losujLiczbe(min, max) {
+                var zakres = max - min + 1;
+                var wylosowanaLiczba = Math.floor(Math.random() * zakres) + min;
+                return wylosowanaLiczba;
+        }
+            var wylosowana = losujLiczbe(min, max);
+            console.log(wylosowana);
+
+            $('#result').text('Wylosowana liczba: ' + wylosowana); 
+            e.preventDefault();
+            }
+    )
+
 });
 
