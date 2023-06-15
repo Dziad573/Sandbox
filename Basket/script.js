0f73fed05ccdf743e4e888e89adf4e65b6af9362
Dziad573 <krystiansmagacz05@gmail.com>
2023-06-15 23:54:42 +0200

    fixed drag event

--- Basket/script.js
+++ Basket/script.js
@@ -1,8 +1,25 @@
 $(document).ready(function(){
-    $(".item").dragstart(function(ev){
-        ev.dataTransfer.setData("text", $(this).attr("id"));
-        $("#move").fadeIn();
-    }).dragend(function(ev){
-        $("move").fadeOut();
+    $(".item").on("dragstart", function(ev){
+        ev.originalEvent.dataTransfer.setData("text", $(this).attr("id"));
+        $("#move").fadeIn(200);
+    }).on("dragend", function(ev){
+        $("#move").fadeOut(200);
+    });
+    var defaultColor = $("#basket").css("background-color");
+    $("#basket").on("dragover", function(ev){
+        ev.preventDefault();
+        $(this).css("background-color", "rgb(147, 176, 204)");
+    }).on("dragleave", function(ev){
+        $(this).css("background-color", defaultColor);
+    }).on("drop", function(ev){
+        ev.preventDefault();
+        $(this).css("background-color", defaultColor);
+        
+        var itemId = ev.originalEvent.dataTransfer.getData("text");
+        var name = $("#" + itemId + ".name").text();
+        var price = $("#" + itemId + ".price").text();
+        
+        var li = "<li class='itemInBasket'><b>" + name + "<b><span>" + price + " zł</li>";
+        $("#basket").append(li);
     });
 });
