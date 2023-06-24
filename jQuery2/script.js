a871b1187f432c2b057a58a1e5db6e6317603138
Dziad573 <krystiansmagacz05@gmail.com>
2023-06-24 22:31:02 +0200

    local store

---
+++ jQuery2/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+
+    function init(){
+        $("form input.stored").each(function(){
+            var form = $(this).parent();
+            var idOfForm = form.attr("id");
+            var type = $(this).data("type");
+
+            if(localStorage.getItem("#" + idOfForm + "input[data-type='"+type+"']"))
+                $(this).val(localStorage.getItem("#" + idOfForm + "input[data-type='"+type+"']"));
+        });
+    }
+
+    init();
+
+    $("input.stored").on("keyup",function(){
+        var form = $(this).parent();
+        var idOfForm = form.attr("id");
+        var type = $(this).data("type");
+
+        localStorage.setItem("#" + idOfForm + "input[data-type='"+type+"']", $(this).val());
+    });
+
+    $("input[type='submit']").on("click", function(){
+        var name = $("input[data-type='name']").val();
+        var eMail = $("input[data-type='E-mail']").val();
+        var tel = $("input[data-type='tel']").val();
+    
+        if(name === "" || eMail === "" || tel === ""){
+            alert("Uzupełnij formularz!");
+        } else if(tel.length === 9) {
+            alert("Dane zostały przesłane!");
+            $("input[data-type='name']").val('');
+            $("input[data-type='E-mail']").val('');
+            $("input[data-type='tel']").val('');
+        } else {
+            alert("Nieprawidłowy numer telefonu!");
+        }
+    });
+    
+    
+});
