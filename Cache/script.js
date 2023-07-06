553e6e0e1f94e4e6f6511ee8d633212c720ae272
Dziad573 <krystiansmagacz05@gmail.com>
2023-07-06 23:17:40 +0200

    canvas

---
+++ Cache/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    var canvas = $("#canv")[0];
+    ctx = canvas.getContext("2d");
+
+    $("#canv").on("mousemove", function(event){
+        var x = countOffsetX(event, $(this));
+        var y = countOffsetY(event, $(this));
+
+        $("#info").html("(" +x+ ", " +y+ ")");
+    });
+
+
+    var i = 0;
+    $("#canv").on("click", function(event){
+        var x = countOffsetX(event, $(this));
+        var y = countOffsetY(event, $(this));
+
+        if(++i %2){
+            ctx.moveTo(x, y);
+        }else{
+            ctx.lineTo(x, y);
+            ctx.strokeStyle=("white");
+            ctx.stroke();
+        }
+    });
+    function countOffsetX(event, object){
+        return event.pageX - object.offset().left;
+    }
+    function countOffsetY(event, object){
+        return event.pageY - object.offset().top;
+    }
+});
